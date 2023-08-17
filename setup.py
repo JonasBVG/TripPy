513bfb7c57d20379074f80a7cd453bab08586394
JonasBVG <107044926+JonasBVG@users.noreply.github.com>
2023-08-17 15:55:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup
 
 setup(
-    # Whatever arguments you need/want
+     # Needed to silence warnings (and to be a worthwhile package)
+    name='Measurements',
+    url='https://github.com/JonasBVG/TripPy.git',
+    author='Jonas Huebner',
+    author_email='jonas.huebner@bvg.de',
+    # Needed to actually package something
+    packages=['trippy'],
+    # Needed for dependencies
+    install_requires=['geopandas'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='GPL-3.0',
+    description='Module to analyze trip tables and to create reports from MATSim runs',
+    
+    # long_description=open('README.txt').read(),
 )

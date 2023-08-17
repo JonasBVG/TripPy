c7ae6e2cecef33fc41cdfd35609364380e83e6c0
JonasBVG <107044926+JonasBVG@users.noreply.github.com>
2023-08-17 16:12:30 +0200

    Update __init__.py

---
+++ TripPy/__init__.py
@@ -0,0 +1,7 @@
+from . import trippy
+
+create_DataFrame = trippy.create_DataFrame
+get_ModalSplit = trippy.get_ModalSplit
+set_Geometry = trippy.set_Geometry
+make_LineString = trippy.make_LineString
+plot_ModalSplit = trippy.plot_ModalSplit

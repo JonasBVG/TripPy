99c3470bb8839874fa05dce57aeffa33f7d4b243
JonasBVG <jonas.huebner@bvg.de>
2023-08-23 14:15:41 +0200

    added Shape-Class and od_agg

--- TripPy/__init__.py
+++ TripPy/__init__.py
@@ -3,5 +3,5 @@ from .trippy import TripTable
 create_DataFrame = TripTable.create_DataFrame
 get_ModalSplit = TripTable.get_ModalSplit
 set_Geometry = TripTable.set_Geometry
-make_LineString = TripTable.make_LineString
+make_LineString = TripTable.create_LineString
 plot_ModalSplit = TripTable.plot_ModalSplit

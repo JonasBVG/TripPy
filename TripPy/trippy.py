80d7ed37c973787ef9bfe5e5f3152285f8830e34
JuliusBVG <julius.goelz@bvg.de>
2023-09-13 17:40:50 +0200

    start writing docstrings

--- TripPy/trippy.py
+++ TripPy/trippy.py
@@ -6,7 +6,7 @@ import json
 class Scenario:
     """
     Class that models a transport planning scenario holding one or more different
-    (geo)pandas (Geo)DataFrames containing data on trips, legs and/or network links
+    `(geo)pandas` `(Geo)DataFrames` containing data on trips, legs and/or network links
     """
 
     def __init__(
@@ -48,7 +48,7 @@ class Scenario:
         self, df: pd.DataFrame, table_name: str
     ) -> bool:
         """
-        Checks a given DataFrame for compliance with table specifications defined in tables_specification.json
+        Checks a given `DataFrame` for compliance with table specifications defined in `tables_specification.json`
         """
         # TODO: Implement column type checking
         # TODO: Implement column type casting
@@ -81,9 +81,10 @@ class Scenario:
 
     def add_trips_df(self, trips_df: pd.DataFrame) -> None:
         """
-        Add a trip table to the scenario
+        Add a trip `DataFrame` to the scenario
         ---
-        trips_df: a pandas DataFrame containing at least one trip
+        Arguments:
+        - `trips_df`: a `pandas` `DataFrame` containing at least one trip
         """
 
         if self.__check_specification_compliance(trips_df, "trips_df"):
@@ -94,9 +95,10 @@ class Scenario:
 
     def add_legs_df(self, legs_df: pd.DataFrame) -> None:
         """
-        Add a leg table to the scenario
+        Add a leg `DataFrame` to the scenario
         ---
-        legs_df: a pandas DataFrame containing at least one leg
+        Arguments:
+        - legs_df: a `pandas` `DataFrame` containing at least one leg
         """
 
         if self.__check_specification_compliance(legs_df, "legs_df"):
@@ -107,9 +109,10 @@ class Scenario:
 
     def add_links_df(self, links_df: pd.DataFrame | gpd.GeoDataFrame) -> None:
         """
-        Add a link table to the scenario
+        Add a link `DataFrame` to the scenario
         ---
-        links_df: a pandas DataFrame containing at least one link
+        Arguments:
+        - `links_df`: a `pandas` `DataFrame` or a `geopandas` `GeoDataFrame` containing at least one link
         """
 
         if self.__check_specification_compliance(links_df, "links_df"):
@@ -119,81 +122,255 @@ class Scenario:
                 self.__links_df["link_id"] = self.__links_df.index.astype(str)
 
     def get_trips_df(self) -> pd.DataFrame:
+        """
+        Get the trips `DataFrame` stored in the scenario
+        ---
+        """
         return self.__trips_df
 
     def get_legs_df(self) -> pd.DataFrame:
+        """
+        Get the legs `DataFrame` stored in the scenario
+        ---
+        """
         return self.__legs_df
 
     def get_links_df(self) -> gpd.GeoDataFrame:
+        """
+        Get the links `DataFrame` stored in the scenario
+        ---
+        """
         return self.__links_df
 
     def __generate_linked_df(self) -> None:
+        """
+        Generate a linked df consisting of trips, legs, links. Not sure if this will be used
+        ---
+        """
         raise NotImplementedError
 
     def get_n_trips(self) -> int:
+        """
+        Get the total number of trips
+        ---
+        """
         raise NotImplementedError
 
     def get_n_persons(self) -> int:
+        """
+        Get the number of unique persons
+        ---
+        """
         raise NotImplementedError
 
     def get_person_km(self) -> float:
+        """
+        Get the total number of person kilometers performed
+        ---
+        """
         raise NotImplementedError
 
     def get_trips_day(self, time_interval: int = 60) -> pd.DataFrame:
+        """
+        Get a `DataFrame` containing the number of trips over the course of the day
+        ---
+        Arguments:
+        - `time_interval`: number of minutes one time bin consists of
+
+        Columns of `DataFrame` returned:
+        - `time_index`: Index of the time interval bin. Example: If `time_interval` was set to 60 mins, there will be indices 0-23
+        - `n`: Number of trips starting in this time bin
+        """
         raise NotImplementedError
 
     def get_modal_split(self, type: str = "volume") -> pd.DataFrame:
+        """
+        Get a `DataFrame` containing the number of trips or the number of person kilometers per mode of transport
+        ---
+        Arguments:
+        - `type`: 'volume' or 'performance'. Using 'volume' will return the number of trips, using 'performance' will return person kilometers
+
+        Columns of `DataFrame` returned:
+        - `mode`: mode of transport
+        - `n`: Number of trips OR Number of person kilometers travelled
+        """
+        raise NotImplementedError
+
+    # Might consider consolidating all these non-time-related/time-bin-related pairs of methods into one method respectively.
+    # Setting time_interval=None then might just lead to aggregating across the whole day
+    def get_modal_split_day(
+        self, type: str = "volume", time_interval: int = 60
+    ) -> pd.DataFrame:
+        """
+        Get a `DataFrame` containing the number of trips or the number of person kilometers per mode of transport
+        ---
+        Arguments:
+        - `type`: 'volume' or 'performance'. Using 'volume' will return the number of trips, using 'performance' will return person kilometers
+        - `time_interval`: number of minutes one time bin consists of
+
+        Columns of DataFrame returned:
+        - `mode`: mode of transport
+        - `time_index`: Index of the time interval bin. Example: If `time_interval` = 60 mins, there will be indices 0-23
+        - `n`: Number of trips starting in this time bin OR Number of person kilometers travelled on trips starting in this time bin
+        """
+        # This should probably not function as described above, but should actually count the person kilometers in the time bin, not of trips starting in this time bin
+        # A long 2h trip starting at 7:59 will completely fall into time bin 6-7 for example
+
         raise NotImplementedError
 
     def get_vehicle_km(self) -> pd.DataFrame:
+        """
+        Get a `DataFrame` containing the total number of vehicle kilometers performed per (vehicle-using) mode
+        ---
+
+        Columns of `DataFrame` returned:
+        - `mode`: mode of transport
+        - `n`: Number of vehicle kilometers performed
+        """
         raise NotImplementedError
 
     def get_travel_time_stats(self) -> pd.DataFrame:
+        """
+        Get a `DataFrame` containing a collection of travel time statistics
+        ---
+
+        Columns of `DataFrame` returned:
+        - `travel_part`: part of travel time the minutes value stands for. Example: 'waiting'
+        - `minutes`: Minutes the travel time part took on average
+        """
         raise NotImplementedError
 
     def get_n_vehicles_day(self, time_interval: int = 60) -> pd.DataFrame:
+        """
+        Get a `DataFrame` containing the number of unique vehicles travelling on the network per time interval
+        ---
+        Arguments:
+        - `time_interval`: number of minutes one time bin consists of
+
+        Columns of `DataFrame` returned:
+        - `mode`: mode of transport
+        - `time_index`: Index of the time interval bin. Example: If `time_interval` = 60 mins, there will be indices 0-23
+        - `n`: Number of vehicles entering at least one link in the respective time bin
+        """
         raise NotImplementedError
 
-    def get_peak_interval(self, time_interval: int = 60, modes: str | list[str] | None = None) -> int:
+    def get_peak_interval(
+        self, time_interval: int = 60, modes: str | list[str] | None = None
+    ) -> int:
+        """
+        Get the index of the time bin with the highest number of trips starting, summarised over the specified mode(s)
+        ---
+        Arguments:
+        - `time_interval`: number of minutes one time bin consists of
+        - `modes`: name of a single mode, a list of mode names or None. If mode(s) are specified, the peak time bin for those modes combined will be calculated. If None, all modes will be summarised
+        """
         raise NotImplementedError
 
     def get_od_agg(
         self,
         agg_gdf: gpd.GeoDataFrame,
+        distinguish_modes=True,
         pt_lines: str | list[str] | None = None,
-    ) -> pd.DataFrame:
+    ) -> gpd.GeoDataFrame:
+        """
+        Get a `GeoDataFrame` containing the number of trips originating/ending in specified zones the trips are aggregated to, optionally per mode and only for specified pt lines
+        ---
+        Arguments:
+        - `agg_gdf`: multipolygon `GeoDataFrame` containing zones the trips will be aggregated to. Must contain a column called `zone_id`
+        - `distinguish_modes`: whether or not to distinguish between modes in the resulting `GeoDataFrame`. Must be set to `True` to use `pt_lines`
+        - `pt_lines`: line id, list of line ids or `None`. If line id(s) are specified, the pt mode(s) will only contain OD data for those lines. If set to `None`, all lines will be included
+
+        Columns of `GeoDataFrame` returned:
+        - `zone_id`: id of the respective zone
+        - `mode`: mode of transport. If `distinguish_modes` is set to `False` this column will contain `nan`s
+        - `n_origin`: Number of trips originating from this zone
+        - `n_destination`: Number of trips ending in this zone
+        """
         raise NotImplementedError
 
     def get_od_agg_day(
         self,
         agg_gdf: gpd.GeoDataFrame,
+        distinguish_modes=True,
         pt_lines: str | list[str] | None = None,
-        time_interval: int = 60
+        time_interval: int = 60,
+    ) -> pd.DataFrame:
+        """
+        Get a `GeoDataFrame` containing the number of trips originating/ending in specified zones the trips are aggregated to per time bin, optionally per mode and only for specified pt lines
+        ---
+        Arguments:
+        - `agg_gdf`: multipolygon `GeoDataFrame` containing zones the trips will be aggregated to. Must contain a column called `zone_id`
+        - `distinguish_modes`: whether or not to distinguish between modes in the resulting `GeoDataFrame`. Must be set to `True` to use `pt_lines`
+        - `pt_lines`: line id, list of line ids or `None`. If line id(s) are specified, the pt mode(s) will only contain OD data for those lines. If set to `None`, all lines will be included
+        - `time_interval`: number of minutes one time bin consists of
+
+        Columns of `GeoDataFrame` returned:
+        - `zone_id`: id of the respective zone
+        - `mode`: mode of transport. If `distinguish_modes` is set to `False` this column will contain `nan`s
+        - `time_index`: Index of the time interval bin. Example: If `time_interval` = 60 mins, there will be indices 0-23
+        - `n_origin`: Number of trips originating from this zone in the respective time bin
+        - `n_destination`: Number of trips ending in this zone in the respective time bin
+        """
+        raise NotImplementedError
+
+    def get_relations_agg(
+        self,
+        agg_gdf: gpd.GeoDataFrame,
+        distinguish_modes=True,
+        pt_lines: str | list[str] | None = None,
+    ) -> gpd.GeoDataFrame:
+        raise NotImplementedError
+
+    def get_relations_agg_day(
+        self,
+        agg_gdf: gpd.GeoDataFrame,
+        distinguish_modes=True,
+        pt_lines: str | list[str] | None = None,
+        time_interval: int = 60,
+    ) -> gpd.GeoDataFrame:
+        raise NotImplementedError
+
+    def get_line_ridership(
+        self,
+        lines: str | list[str] | None = None,
     ) -> pd.DataFrame:
         raise NotImplementedError
 
-    def get_line_ridership(self, lines: str | list[str] | None = None,) -> pd.DataFrame:
+    def get_line_ridership_day(
+        self, lines: str | list[str] | None = None, time_interval: int = 60
+    ) -> pd.DataFrame:
         raise NotImplementedError
 
-    def get_line_ridership_day(self, lines: str | list[str] | None = None, time_interval: int = 60) -> pd.DataFrame:
+    def get_line_occupancy(
+        self,
+        lines: str | list[str] | None = None,
+    ) -> pd.DataFrame:
         raise NotImplementedError
 
-    def get_line_occupancy(self, lines: str | list[str] | None = None,) -> pd.DataFrame:
+    def get_line_occupancy_day(
+        self, lines: str | list[str] | None = None, time_interval: int = 60
+    ) -> pd.DataFrame:
         raise NotImplementedError
 
-    def get_line_occupancy_day(self, lines: str | list[str] | None = None, time_interval: int = 60) -> pd.DataFrame:
+    def get_line_stops(
+        self,
+        lines: str | list[str] | None = None,
+    ) -> gpd.GeoDataFrame:
         raise NotImplementedError
 
-    def get_line_stops(self, lines: str | list[str] | None = None,) -> gpd.GeoDataFrame:
+    def get_line_stops_day(
+        self, lines: str | list[str] | None = None, time_interval: int = 60
+    ) -> gpd.GeoDataFrame:
         raise NotImplementedError
 
-    def get_line_stops_day(self, lines: str | list[str] | None = None, time_interval: int = 60) -> gpd.GeoDataFrame:
+    def get_line_links_occupancy(
+        self, lines: str | list[str] | None = None
+    ) -> gpd.GeoDataFrame:
         raise NotImplementedError
 
-    def get_line_links_occupancy(self,  lines: str | list[str] | None = None) -> gpd.GeoDataFrame:
-        raise NotImplementedError
-
-    def get_line_links_occupancy_day(self, lines: str | list[str] | None = None, time_interval: int = 60) -> gpd.GeoDataFrame:
+    def get_line_links_occupancy_day(
+        self, lines: str | list[str] | None = None, time_interval: int = 60
+    ) -> gpd.GeoDataFrame:
         raise NotImplementedError
 
 

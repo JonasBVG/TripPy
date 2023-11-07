d9d80083299cf66c71d9c61ddb1418869ded19d4
JuliusBVG <julius.goelz@bvg.de>
2023-11-07 16:13:52 +0100

    rename reporter.py to report.py

--- trippy/__init__.py
+++ trippy/__init__.py
@@ -2,5 +2,5 @@ from .scenario import Scenario
 from .drtscenario import DRTScenario
 from .comparison import Comparison
 from .visualizer import Visualizer
-from .reporter import Report
+from .report import Report
 from .trippy import *
